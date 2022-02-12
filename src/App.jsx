495bd5b97557f8d562528f837becb9238bf039ce
unknown <fariborz.miad22222222@gmail.com>
2022-02-12 12:13:45 +0330

    css converted to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/header/header";
 function App() {
   return (
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import logo from "../../img/logo-white.png";
 
-import "./header.css"
+import "./header.scss"
 
 function header() {
   return (
     <div>
          <header className="header">
-        <div className="logo-box">
-          <img src={logo} alt="logo" className="logo" />
+        <div className="header__logo-box">
+          <img src={logo} alt="logo" className="header__logo" />
         </div>
-        <div className="text-box">
+        <div className="header__text-box">
           <h1 className="heading-primary">
             <span className="heading-primary-main">Outdoors</span>
             <span className="heading-primary-sub">is where life happens</span>
           </h1>
-          <a href="#home"className="btn btn-white btn-animated">
+          <a href="#home"className="btn btn--white btn--animated">
             Discover our tours
           </a>
         </div>
