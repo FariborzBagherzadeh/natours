d8f1bfdc1eab808b28ddb0e407b00d90c08b27e0
unknown <fariborz.miad22222222@gmail.com>
2022-02-13 11:02:43 +0330

    about section part 2

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,22 +5,38 @@ import "./about.scss";
 function about() {
   return (
     <section className="section-about">
-      <div className="u-center-text u-margin-bottom-8">
+      <div className="u-center-text u-margin-bottom-big">
         <h2 className="heading-secondary">
           Exciting tour for adveturous people
         </h2>
       </div>
       <Grid
-      className="section-about-grid"
+      
+        className="section-about-grid"
         container
-        columnSpacing={6}
+        columnSpacing={12}
         rowSpacing={12}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
       >
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          Text1
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            You're going to fall in love with nature
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
+            sequi tempore quis deserunt sed ratione distinctio maiores possimus
+            dolorum, earum voluptatibus? Quas, optio labore. Adipisci vitae
+            autem qui maxime consequatur?
+          </p>
+          <h3 className="heading-tertiary u-margin-bottom-small">
+            Live adventures like you never have before
+          </h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
+            sequi tempore quis deserunt sed ratione distinctio.
+          </p>
+          <a href="#home" className="btn-text">
+            Learn More &rarr;
+          </a>
         </Grid>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           Text1
